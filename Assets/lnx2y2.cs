0eb2c4f7c262712d4cd963ff3e242e3d24afb795
DamianoP <damiano.perri@gmail.com>
2023-10-31 17:59:21 +0100

    20231131

---
+++ Assets/lnx2y2.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class lnx2y2 : MonoBehaviour {
+  
+	private int resolution = 80;
+	public Material mat;
+	private List<Vector3> points=new List<Vector3>();
+	private List<int> triangles = new List<int>();
+	private GameObject figure;	
+	private MeshSuperclass meshHelper;
+	void Start (){
+		meshHelper = new MeshSuperclass ();
+		int counter = 0;
+		resolution=80; 
+		int[,] matrixIndices = new int[resolution, resolution];
+		for (int i = 0; i < resolution; i++) {
+			for (int j = 0; j < resolution; j++) {
+				matrixIndices [i, j] = counter;
+				counter++;
+			}
+		}
+		float x = 0, y = 0, z = 0, x1 = 0, y1 = 0, z1 = 0;
+		float step = 0.1f;
+		for (int i = 0; i < resolution; i++) {
+			for (int j = 0; j < resolution; j++) {
+				x = -4+(i*step);
+				z = -4+(j*step);
+				y =getY(x,z);
+				points.Add (new Vector3 (x, y, z));
+
+				if (i < resolution - 1 && j < resolution - 1) {
+					/*
+	            ij    ij1
+	            i1j   i1j1
+	          */
+					triangles.Add (matrixIndices [i, j]);
+					triangles.Add (matrixIndices [i, j + 1]);          
+					triangles.Add (matrixIndices [i + 1, j]);  
+
+					triangles.Add (matrixIndices [i + 1, j]);
+					triangles.Add (matrixIndices [i, j + 1]);
+					triangles.Add (matrixIndices [i + 1, j + 1]);
+				}
+			}
+		}
+		figure = gameObject;// GameObject ("3dMesh");
+		//figure.AddComponent<MeshRenderer> ();
+		//figure.AddComponent<MeshFilter> ();
+		figure.GetComponent<MeshFilter> ().mesh = meshHelper.get3Dmesh (points, triangles);
+		figure.GetComponent<MeshRenderer> ().material = mat;
+		figure.transform.SetPositionAndRotation (transform.position, new Quaternion ());
+        //figure.AddComponent<Light> ().color = Color.blue;
+        //threeDSquare.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.blue);
+        //meshHelper.saveMesh(figure.GetComponent<MeshFilter>().mesh, "bucoNero");
+    }
+
+	float getY(float x,float y){
+		float x2=x*x;
+		float y2=y*y;
+		return  Mathf.Log(x2+y2);
+	}
+}
---
+++ Assets/triangoloSfera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triangoloSfera : MonoBehaviour
+{
+	private GameObject figure;
+    private float z = 0.0001f;
+    private int increment=1;
+    private float transformSpeed=0.6f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+    	figure= gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(increment==1)
+    		z+=Time.deltaTime*transformSpeed;
+    	else
+    		z-=Time.deltaTime*transformSpeed;
+    	if(z>2)
+    		increment=0;
+    	if(z<0)	
+    		increment=1;
+        transform.localScale = new Vector3(2f, 2f, z);
+    }
+}
